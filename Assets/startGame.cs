b6d150a8aaacfbd830876d3d0eff47e9e29f6ecd
Xan1237 <xn543440@dal.ca>
2024-01-27 13:34:52 -0400

    Main menu functionality mostly done
    buttons all work make sure to set player prefabs on the completion of levels

--- Assets/startGame.cs
+++ Assets/startGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StartButton : MonoBehaviour
@@ -10,6 +11,36 @@ public class StartButton : MonoBehaviour
 
     }
 
+    public void Button1(){
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void Button2(){
+        if(PlayerPrefs.GetInt("LevelOne") == 1){
+        SceneManager.LoadScene(3);
+        }
+    }
+        public void Button3(){
+        if(PlayerPrefs.GetInt("LevelOne") == 1 && PlayerPrefs.GetInt("LevelTwo") ==1){
+        SceneManager.LoadScene(4);
+        }
+        }
+        public void Button4(){
+            if(PlayerPrefs.GetInt("LevelOne") == 1 && PlayerPrefs.GetInt("LevelTwo") ==1 && PlayerPrefs.GetInt("LevelThree") == 1){
+            SceneManager.LoadScene(5);
+        }
+    }
+        public void Button5(){
+            if(PlayerPrefs.GetInt("LevelOne") == 1 && PlayerPrefs.GetInt("LevelTwo") ==1 && PlayerPrefs.GetInt("LevelThree") == 1 && PlayerPrefs.GetInt("LevelFour") == 1){
+            SceneManager.LoadScene(6);
+        }
+    }
+    public void Button6(){
+        if(PlayerPrefs.GetInt("LevelOne") == 1 && PlayerPrefs.GetInt("LevelTwo") ==1 && PlayerPrefs.GetInt("LevelThree") == 1 && PlayerPrefs.GetInt("LevelFour")==1 && PlayerPrefs.GetInt("LevelFive")==1){
+        SceneManager.LoadScene(7);
+        }
+    }
 
 
 }
