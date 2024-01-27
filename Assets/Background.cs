ce3d3476525267a958179c820a6bdc15a551e332
Wild E <wileyswikit@gmail.com>
2024-01-27 18:32:08 -0400

    curtains

--- Assets/Background.cs
+++ Assets/Background.cs
@@ -20,7 +20,7 @@ public class Background : MonoBehaviour
             {
                 GameObject row = Instantiate(_rowPrefab, transform);
 
-                float x = (i * 25f) + (j % 2 == 0 ? 0 : 0.8f) - 20f;
+                float x = (i * 35f) + (j % 2 == 0 ? 0 : 0.8f) - 25f;
                 float y = 10f + j * -1.25f;
 
                 row.transform.localPosition = new Vector2(x, y);
