1769a4b1e2790b99c26d6be6d3f2236e7f906279
jhayes2000 <157842861+jhayes2000@users.noreply.github.com>
2024-01-28 11:15:46 -0400

    Level 6 fix

---
+++ Assets/AudioTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio()
+    {
+        _audioSource.Play();
+    }
+}
